8fd0480b8649c850eefd1389ecf9381123f2eabb
Steven Tran <StevenDoTran@gmail.com>
2023-11-20 22:56:34 -0600

    Followed tutorial up to Section 3-53

--- Scenes/UI/ability_upgrade_card.gd
+++ Scenes/UI/ability_upgrade_card.gd
@@ -5,9 +5,23 @@ signal selected
 @onready var name_label = $%NameLabel
 @onready var description_label = $%DescriptionLabel
 
+var disabled = false
 
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+	GameEvents.all_upgrade_cards_loaded.connect(on_all_upgrade_cards_loaded)
+
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	scale = Vector2.ZERO
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+
+
+func play_discard():
+	$AnimationPlayer.play("discard")
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
@@ -15,6 +29,37 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 	description_label.text = upgrade.description
 
 
+func select_card():
+	disabled = true
+	$AnimationPlayer.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+			
+		other_card.play_discard()
+	
+	await $AnimationPlayer.animation_finished
+	selected.emit()
+
+
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
+	
 	if Input.is_action_just_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+func on_mouse_entered():
+	if disabled:
+		return
+		
+	$HoverAnimationPlayer.play("hover")
+
+
+func on_all_upgrade_cards_loaded():
+	# gives a little delay after all the cards are shown before player can click on them
+	# to prevent spam misclicking
+	await get_tree().create_timer(.1).timeout
+	mouse_filter = Control.MOUSE_FILTER_STOP
--- Scenes/UI/end_screen.gd
+++ Scenes/UI/end_screen.gd
@@ -1,9 +1,23 @@
 extends CanvasLayer
 
-@onready var title_label = $%TitleLabel
-@onready var description_label = $%DescriptionLabel
+@onready var title_label = %TitleLabel
+@onready var description_label = %DescriptionLabel
+@onready var panel_container = %PanelContainer
 
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2
+	panel_container.scale = Vector2.ZERO
+	
+	var tween = create_tween()
+	# need a tween setting scale to zero in 0 seconds due to a Godot quirk
+	# tweens wont happen until the scale is reset (back to (1, 1))
+	# and so in between us setting scale to (0, 0) and creating the tween
+	# to go to (1, 1), godot resets the scale
+	# this is just a workaround
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	$%RestartButton.pressed.connect(on_restart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
